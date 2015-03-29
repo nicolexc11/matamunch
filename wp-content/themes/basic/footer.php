1f88dfbe3b05bedd4ef5b4936dea57265f6f9da8
MaggieM28 <maddybaddy1291@Magdalenas-MacBook-Pro-3.local>
2015-03-29 13:03:54 -0700

    swipe-able navbar
    The navigation in a form of a navbar that slides out when clicked on or
    swiped when in mobile

--- wp-content/themes/basic/footer.php
+++ wp-content/themes/basic/footer.php
@@ -1,7 +1,9 @@
 	<div id="footer">
 		<?php wp_footer(); ?>
 	</div>
-      
+
+</div><!-- x main content -->     
+</div><!-- x content -->
 </div><!-- x content -->
 </body>
 </html>
--- wp-content/themes/basic/header.php
+++ wp-content/themes/basic/header.php
@@ -1,13 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.touchSwipe.min.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/sidebar.js"></script>
 	<?php wp_head();?>
 </head>
 <body>
   <div class="container">
+
+  <div id="sidebar">
+		<ul>
+			<li class="<?php is_active('about'); ?>"><a href="<?php bloginfo('url'); ?>/about">Login/Logout</a></li>
+			<li class="<?php ?>"><a href="<?php bloginfo('url'); ?>">Home</a></li>
+			<li class="<?php is_active('category'); ?>"><a href="<?php bloginfo('url'); ?>/category">Category</a></li>
+			<li class="<?php is_active('areas'); ?>"><a href="<?php bloginfo('url'); ?>/areas">Areas</a></li>
+			<li class="<?php is_active('directory'); ?>"><a href="<?php bloginfo('url'); ?>/directory">Directory</a></li>
+			<li class="<?php is_active('hours'); ?>"><a href="<?php bloginfo('url'); ?>/hours">Hours</a></li>
+		</ul>
+	</div>
+
+	<div class="main-content">
+		<div class="swipe-area"></div>
+		<a href="#" data-toggle=".container" id="sidebar-toggle">
+			<span class="bar"></span>
+			<span class="bar"></span>
+			<span class="bar"></span>
+		</a>
+	
+	
+	<div class="header">
+		<div class="logo"><h1>MataMunch</h1></div>
+	</div>
+	<div class="content row">
